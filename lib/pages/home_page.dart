48b7f30755539f5f8f17889b9efa979c51b65e8b
DiyaShajith <diyashajith.2231@gmail.com>
2024-08-13 09:11:44 +0530

    portfolio_homepage

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/constants/colors.dart';
+import 'package:portfolio/constants/nav_items.dart';
+import 'package:portfolio/widgets/header_desktop.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: CustomColor.scaffoldBg,
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: [
+          // MAIN
+          const HeaderDesktop(),
+          // SKILLS
+          Container(
+            height: 500,
+            width: double.maxFinite,
+            color: Colors.blueGrey,
+          ),
+          // PROJECTS
+          const SizedBox(
+            height: 500,
+            width: double.maxFinite,
+          ),
+          // CONTACT
+          Container(
+            height: 500,
+            width: double.maxFinite,
+            color: Colors.blueGrey,
+          ),
+          // FOOTER
+          const SizedBox(
+            height: 500,
+            width: double.maxFinite,
+          )
+        ],
+      ),
+    );
+  }
+}
